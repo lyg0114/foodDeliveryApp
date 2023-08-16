07db19f13b53ec2b413963629a29452b157c23d9
lyg0114 <whdnseowkd@naver.com>
2023-08-16 20:34:17 +0900

    Add SignIn

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,29 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useCallback } from "react";
+import { Alert, Pressable, Text, TextInput, View } from "react-native";
 
 function SignIn() {
-  return <View>
-    <Text>로그인</Text>
-  </View>;
+
+  const onSubmit = useCallback(() => {
+    Alert.alert("알림", "안녕~");
+  }, []);
+
+  return (
+    <View>
+      <View>
+        <Text>이메일</Text>
+        <TextInput placeholder="이메일을 입력해주세요." />
+      </View>
+      <View>
+        <Text>비밀번호</Text>
+        <TextInput placeholder="비밀번호를 입력해주세요." />
+      </View>
+      <View>
+        <Pressable onPress={onSubmit}>
+          <Text>로그인</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 export default SignIn;
