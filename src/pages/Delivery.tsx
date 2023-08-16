8f4134ca6140029021385efc3a18a8972ade49e6
lyg0114 <whdnseowkd@naver.com>
2023-08-16 20:19:56 +0900

    Add : add stack.screen

--- src/pages/Delivery.tsx
+++ src/pages/Delivery.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Ing from "./Ing";
+import Complete from "./Complete";
+
+const Stack = createNativeStackNavigator();
 
 function Delivery() {
-  return <View>
-    <Text>주문</Text>
-  </View>;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Ing" component={Ing} options={{ headerShown: false }} />
+      <Stack.Screen name="Complete" component={Complete} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
 }
 
 export default Delivery;
